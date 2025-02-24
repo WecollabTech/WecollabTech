e7938ca83d936e7485e2c575653583493c9f8334
WecollabTech <victorgomez@we-collab.tech>
2025-02-24 11:51:05 -0600

    Ingrese texto de Bienvenidos

--- index.php
+++ index.php
@@ -6,6 +6,43 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Hola bienvenidos a SGT</h1>
+    <h1 class="Hola">Hola bienvenidos a SGT</h1>
 </body>
 </html>
+
+<style>
+    .Hola {
+        color: red;
+        font-size: 24px;
+    }
+
+    /* Añadimos la regla para el hover */
+    .Hola:hover {
+        color: blue;
+    }
+
+    /* Añadimos la regla para el active */
+    .Hola:active {
+        color: green;
+    }
+
+    /* Añadimos la regla para el focus */
+    .Hola:focus {
+        color: yellow;
+    }
+
+    /* Añadimos la regla para el visited */
+    .Hola:visited {
+        color: purple;
+    }
+
+    /* Añadimos la regla para el link */
+    .Hola:link {
+        color: orange;
+    }
+
+    /* Añadimos la regla para el visited */
+    .Hola:visited {
+        color: pink;
+    }
+</style>
